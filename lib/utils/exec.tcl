6055356c1d3d7daf8d01d857ba94c173cdfa0f2a
Andreas Kupries <Andreas Kupries>
2018-11-09 22:35:53 +0000

    Tkt [21d30bb18f] VCS: capture stdout/err of store operations.
    Added tests for the base capture management facilities.
    Added more arg validation to the management commands.
    
    Added internal comments describing the (intended) new behaviour of go,
    get, and silent.
    
    Implemented capture for go, get, and silent.
    
    Capture paths untested (implied testing of some of the non-capture
    paths by the existing m-* tests).

--- lib/utils/exec.tcl
+++ lib/utils/exec.tcl
@@ -51,18 +51,24 @@ namespace eval ::m::exec::capture {
 
 proc ::m::exec::capture::to {stdout stderr {enable 1}} {
     debug.m/exec {}
+    B $enable
     # Set clear capture destinations, and start (default).
-    variable out $stdout
-    variable err $stderr
+    # Note: Be independent of future CWD changes.
+    variable out [file normalize $stdout]
+    variable err [file normalize $stderr]
     clear
-    variable active  $enable
+    variable active $enable
     return
 }
 
-proc ::m::exec::capture::off {} {
+proc ::m::exec::capture::off {{reset 0}} {
     debug.m/exec {}
     # Stop capture.
+    B $reset
     variable active 0
+    if {!$reset} return
+    variable out {}
+    variable err {}
     return
 }
 
@@ -88,10 +94,11 @@ proc ::m::exec::capture::clear {} {
     return
 }
 
-proc ::m::exec::capture::get {var} {
+proc ::m::exec::capture::get {key} {
     # Get captured content
     debug.m/exec {}
-    set path [path $var]
+    V $key
+    set path [P $key]
     if {$path eq {}} return
     set c [open $path r]
     set d [read $c]
@@ -99,12 +106,11 @@ proc ::m::exec::capture::get {var} {
     return $d
 }
 
-proc ::m::exec::capture::path {var} {
+proc ::m::exec::capture::path {key} {
     # Get path of capture buffer
     debug.m/exec {}
-    variable $var
-    upvar  0 $var path
-    return $path
+    V $key
+    return [P $key]
 }
 
 proc ::m::exec::capture::active {} {
@@ -114,14 +120,38 @@ proc ::m::exec::capture::active {} {
     return  $active
 }
 
-proc ::m::exec::capture::C {var} {
+proc ::m::exec::capture::P {key} {
+    # Get path of capture buffer
     debug.m/exec {}
-    variable $var
-    upvar 0 $var path
+    variable $key
+    upvar  0 $key path
+    return $path
+}
+
+proc ::m::exec::capture::C {key} {
+    debug.m/exec {}
+    variable $key
+    upvar 0 $key path
     if {$path eq {}} return
     # open for writing, truncates.
     close [open $path w]
     return
+}
+
+proc ::m::exec::capture::V {key} {
+    debug.m/exec {}
+    if {$key in {out err}} return
+    return -code error \
+	    -errorcode {M EXEC CAPTURE BAD KEY} \
+	    "Bad channel key $key"
+}
+
+proc ::m::exec::capture::B {x} {
+    debug.m/exec {}
+    if {[string is boolean -strict $x]} return
+    return -code error \
+	    -errorcode {M EXEC CAPTURE BAD BOOL} \
+	    "Expected boolean, got \"$x\""
 }
 
 # # ## ### ##### ######## ############# #####################
@@ -140,11 +170,29 @@ proc ::m::exec::verbose {{newvalue {}}} {
 proc ::m::exec::go {args} {
     debug.m/exec {}
     variable verbose
+    variable capture::active
+
+    # V C |
+    # ----+-
+    # 0 0 | (a) null
+    # 0 1 | (b) capture
+    # 1 0 | (c) pass to inherited out/err
+    # 1 1 | (d) capture, pass to inherited
+
     if {$verbose} {
+	# c, d
 	m msg "> $args"
+    }
+    if {$active} {
+	# b, d
+	CAP $args $verbose $verbose
+	# d - verbose ^----^
+    } elseif {$verbose} {
+	# c
 	exec 2>@ stderr >@ stdout {*}$args
     } else {
-	exec 2>  [NULL] >  [NULL] {*}$args
+	# a
+	exec 2> [NULL] > [NULL] {*}$args
     }
     return
 }
@@ -152,10 +200,30 @@ proc ::m::exec::go {args} {
 proc ::m::exec::get {args} {
     debug.m/exec {}
     variable verbose
+    variable capture::active
+
+    # V C |
+    # ----+-
+    # 0 0 | (a) null to stderr, return stdout
+    # 0 1 | (b) capture, return stdout
+    # 1 0 | (c) pass to stderr, return stdout
+    # 1 1 | (d) capture, return stdout, pass stderr
+    
     if {$verbose} {
+	# (c)
 	m msg "> $args"
+    }
+
+    if {$active} {
+	# b, d
+	lassign [CAP $args 0 $verbose] oc ec
+	# d - verbose -------^
+	return $oc
+    } elseif {$verbose} {
+	# c
 	return [exec 2>@ stderr {*}$args]
     } else {
+	# a
 	return [exec 2>  [NULL] {*}$args]
     }
 }
@@ -163,10 +231,74 @@ proc ::m::exec::get {args} {
 proc ::m::exec::silent {args} {
     debug.m/exec {}
     variable verbose
+    variable capture::active
+
+    # V C |
+    # ----+-
+    # 0 0 | (a) null
+    # 0 1 | (b) capture
+    # 1 0 | (c) null
+    # 1 1 | (d) capture
+    # ----> a == c
+    # ----> b == d
+    
     if {$verbose} {
+	# c, d
 	m msg "> $args"
     }
-    exec 2> [NULL] > [NULL] {*}$args
+    if {$active} {
+	# b, d
+	set o [capture path out]
+	set e [capture path err]
+	exec 2> $o > $e {*}$args
+    } else {
+	# a, c
+	exec 2> [NULL] > [NULL] {*}$args
+    }
+    return
+}
+
+proc ::m::exec::CAP {cmd vo ve} {
+    # Note: Temp files capture just current execution,
+    #       Main capture then extended from these.
+
+    set o [capture path out]
+    set e [capture path err]
+    try {
+	exec 2> $o.now > $e.now {*}$cmd
+    } finally {
+	set oc [POST $o.now $o $vo stdout]
+	set ec [POST $e.now $e $ve stderr]
+    }
+
+    list $oc $ec
+}
+
+proc ::m::exec::POST {p pe v std} {
+    set d [CAT $p]
+    APPEND $pe $d
+    file delete $p
+    if {$v} { PASS $std $d }
+    return $d
+}
+
+proc ::m::exec::APPEND {path data} {
+    set c [open $path a]
+    puts -nonewline $c $data
+    close $c
+    return
+}
+
+proc ::m::exec::CAT {path} {
+    set c [open $path r]
+    set d [read $c]
+    close $c
+    return $d
+}
+
+proc ::m::exec::PASS {c d} {
+    puts -nonewline $c $d
+    flush $c
     return
 }
 
