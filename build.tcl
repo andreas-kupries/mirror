83a8a5f7f206c2c0ec7563deb634310d14e1427a
Andreas Kupries <Andreas Kupries>
2018-11-22 08:34:10 +0000

    Second github helper `gh-follow`.
    A level above `gh-pull` it explores the outbound neighbourhood of the
    specified user U, generating repository lists for all the people U is
    following.
    
    Makes use of `git hub` to script the github API, like `gh-pull`.
    
    Both helpers are now installed with the main mirror applications.

--- build.tcl
+++ build.tcl
@@ -4,3 +4,8 @@ exec kettle -f "$0" "${1+$@}"
 kettle tcl
 kettle tclapp bin/mirror-migrate
 kettle tclapp bin/mirror
+#
+# Helpers for working with github
+#
+kettle tclapp bin/gh-pull
+kettle tclapp bin/gh-follow
